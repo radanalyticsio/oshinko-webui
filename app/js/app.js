2d90259454c36fec74620e8f4192c03d11a58a46
Chad Roberts <croberts@redhat.com>
2017-04-25 12:26:38 -0400

    Style refactoring to elininate jshint warnings. (#62)
    Fixing up files to honor .jshintrc.  Removing .jscsrc as it's not
    used by anything.

--- app/js/app.js
+++ app/js/app.js
@@ -9,38 +9,40 @@
 
 
 var app = angular.module('Oshinko', [
-    'ngRoute',
-    'Oshinko.controllers',
-    'Oshinko.factories'
+  'ngRoute',
+  'Oshinko.controllers',
+  'Oshinko.factories'
 ]);
 
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/clusters/:Id?',
-        {
-            templateUrl: function(params) {
-                if(!params['Id'])
-                    return 'partials/clusters.html';
-                else
-                    return 'partials/cluster-detail.html';
-            },
-            controller: 'ClusterCtrl',
-            activetab: 'clusters'
-        });
-    $routeProvider.when('/login',
-        {
-            templateUrl: 'partials/login.html',
-            controller: 'LoginController',
-            activetab: ''
-        });
-    $routeProvider.otherwise({redirectTo: '/clusters'});
+  $routeProvider.when('/clusters/:Id?',
+    {
+      templateUrl: function (params) {
+        if (!params.Id) {
+          return 'partials/clusters.html';
+        }
+        else {
+          return 'partials/cluster-detail.html';
+        }
+      },
+      controller: 'ClusterCtrl',
+      activetab: 'clusters'
+    });
+  $routeProvider.when('/login',
+    {
+      templateUrl: 'partials/login.html',
+      controller: 'LoginController',
+      activetab: ''
+    });
+  $routeProvider.otherwise({redirectTo: '/clusters'});
 }]);
 
 app.run(function ($http) {
   window.__env = {};
   $http.get('/config/refresh').success(function (result) {
     window.__env.refresh_interval = result;
-  }).error(function(error) {
-    console.log("Unable to fetch refresh interval, using default of 5");
+  }).error(function (error) {
+    console.log("Unable to fetch refresh interval, using default of 5. " + error);
     window.__env.refresh_interval = 5;
   });
 });
--- app/js/factories.js
+++ app/js/factories.js
@@ -9,162 +9,169 @@
 var module = angular.module('Oshinko.factories', ['ui.bootstrap', 'patternfly.notification']);
 
 module.factory('clusterActions', [
-        '$uibModal',
-        function($uibModal) {
-            function deleteCluster(clusterName) {
-                return $uibModal.open({
-                    animation: true,
-                    controller: 'ClusterDeleteCtrl',
-                    templateUrl: '/forms/' + 'delete-cluster.html',
-                    resolve: {
-                        dialogData: function() {
-                            return { clusterName: clusterName };
-                        }
-                    },
-                }).result;
-            }
-            function newCluster() {
-                return $uibModal.open({
-                    animation: true,
-                    controller: 'ClusterNewCtrl',
-                    templateUrl: '/forms/' + 'new-cluster.html',
-                    resolve: {
-                        dialogData: function() {
-                            return { };
-                        }
-                    },
-                }).result;
-            }
-            function scaleCluster(cluster) {
-                return $uibModal.open({
-                    animation: true,
-                    controller: 'ClusterDeleteCtrl',
-                    templateUrl: '/forms/' + 'scale-cluster.html',
-                    resolve: {
-                        dialogData: function() {
-                            return { clusterName: cluster.name,
-                                workerCount: cluster.workerCount };
-                        }
-                    },
-                }).result;
-            }
-        return {
-                deleteCluster: deleteCluster,
-                newCluster: newCluster,
-                scaleCluster: scaleCluster,
-            };
+  '$uibModal',
+  function ($uibModal) {
+    function deleteCluster(clusterName) {
+      return $uibModal.open({
+        animation: true,
+        controller: 'ClusterDeleteCtrl',
+        templateUrl: '/forms/' + 'delete-cluster.html',
+        resolve: {
+          dialogData: function () {
+            return {clusterName: clusterName};
+          }
         }
-    ]);
+      }).result;
+    }
+
+    function newCluster() {
+      return $uibModal.open({
+        animation: true,
+        controller: 'ClusterNewCtrl',
+        templateUrl: '/forms/' + 'new-cluster.html',
+        resolve: {
+          dialogData: function () {
+            return {};
+          }
+        }
+      }).result;
+    }
+
+    function scaleCluster(cluster) {
+      return $uibModal.open({
+        animation: true,
+        controller: 'ClusterDeleteCtrl',
+        templateUrl: '/forms/' + 'scale-cluster.html',
+        resolve: {
+          dialogData: function () {
+            return {
+              clusterName: cluster.name,
+              workerCount: cluster.workerCount
+            };
+          }
+        }
+      }).result;
+    }
+
+    return {
+      deleteCluster: deleteCluster,
+      newCluster: newCluster,
+      scaleCluster: scaleCluster
+    };
+  }
+]);
 
 module.factory('clusterData', [
-        '$http',
-        function($http) {
-             var urlBase = 'api';
-            function sendDeleteCluster(clusterName) {
-                return $http({
-                    method: "DELETE",
-                    url: urlBase + '/clusters/' + clusterName,
-                    data: '',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'allow-access-control-origin': "*"
-                    }
-                });
-            }
-            function sendCreateCluster(clusterName, workerCount, configName, masterConfigName, workerConfigName) {
-                var jsonData = {
-                    "config": {
-                        "masterCount": 1,
-                        "workerCount": workerCount,
-                        "clusterconfig": configName,
-                        "masterconfig": masterConfigName,
-                        "workerconfig": workerConfigName
-                     },
-                     "name": clusterName
-                }
-                return $http.post(urlBase + "/clusters", jsonData);
-            }
-            function sendScaleCluster(clusterName, workerCount) {
-                var jsonData = {
-                    "config": {
-                        "masterCount": 1,
-                        "workerCount": workerCount
-                    },
-                    "name": clusterName
-                }
-                return $http.put(urlBase + '/clusters/' + clusterName, jsonData);
-            }
-        return {
-                sendDeleteCluster: sendDeleteCluster,
-                sendCreateCluster: sendCreateCluster,
-                sendScaleCluster: sendScaleCluster,
-            };
-        }
-    ]);
-
-module.factory('clusterDataFactory', function($rootScope, $http, sendNotifications) {
+  '$http',
+  function ($http) {
     var urlBase = 'api';
-    var dataFactory = {};
 
-    dataFactory.getClusters = function() {
-        return $http.get(urlBase + "/clusters");
-    };
-
-    dataFactory.getCluster = function(id) {
-        return $http.get(urlBase + '/clusters/' + id);
-    };
-
-    dataFactory.deleteCluster = function(id) {
-        var result = $http({
-            method: "DELETE",
-            url: urlBase + '/clusters/' + id,
-            data: '',
-            headers: {
-                'Content-Type': 'application/json',
-                'allow-access-control-origin': "*"
-            }
-        });
-        return result;
-    };
-
-    dataFactory.createCluster = function(name, workerCount) {
-        var jsonData = {
-            "config": {
-                "masterCount": 1,
-                "workerCount": workerCount
-            },
-            "name": name
+    function sendDeleteCluster(clusterName) {
+      return $http({
+        method: "DELETE",
+        url: urlBase + '/clusters/' + clusterName,
+        data: '',
+        headers: {
+          'Content-Type': 'application/json',
+          'allow-access-control-origin': "*"
         }
-        return $http.post(urlBase + "/clusters", jsonData);
-    };
+      });
+    }
 
-    dataFactory.updateCluster = function(id, name, workerCount) {
-        var jsonData = {
-            "config": {
-                "masterCount": 1,
-                "workerCount": workerCount
-            },
-            "name": name
-        }
-        var result = $http.put(urlBase + '/clusters/' + id, jsonData);
-        return result;
+    function sendCreateCluster(clusterName, workerCount, configName, masterConfigName, workerConfigName) {
+      var jsonData = {
+        "config": {
+          "masterCount": 1,
+          "workerCount": workerCount,
+          "clusterconfig": configName,
+          "masterconfig": masterConfigName,
+          "workerconfig": workerConfigName
+        },
+        "name": clusterName
+      };
+      return $http.post(urlBase + "/clusters", jsonData);
+    }
+
+    function sendScaleCluster(clusterName, workerCount) {
+      var jsonData = {
+        "config": {
+          "masterCount": 1,
+          "workerCount": workerCount
+        },
+        "name": clusterName
+      };
+      return $http.put(urlBase + '/clusters/' + clusterName, jsonData);
+    }
+
+    return {
+      sendDeleteCluster: sendDeleteCluster,
+      sendCreateCluster: sendCreateCluster,
+      sendScaleCluster: sendScaleCluster
     };
-    return dataFactory;
+  }
+]);
+
+module.factory('clusterDataFactory', function ($rootScope, $http) {
+  var urlBase = 'api';
+  var dataFactory = {};
+
+  dataFactory.getClusters = function () {
+    return $http.get(urlBase + "/clusters");
+  };
+
+  dataFactory.getCluster = function (id) {
+    return $http.get(urlBase + '/clusters/' + id);
+  };
+
+  dataFactory.deleteCluster = function (id) {
+    return $http({
+      method: "DELETE",
+      url: urlBase + '/clusters/' + id,
+      data: '',
+      headers: {
+        'Content-Type': 'application/json',
+        'allow-access-control-origin': "*"
+      }
+    });
+  };
+
+  dataFactory.createCluster = function (name, workerCount) {
+    var jsonData = {
+      "config": {
+        "masterCount": 1,
+        "workerCount": workerCount
+      },
+      "name": name
+    };
+    return $http.post(urlBase + "/clusters", jsonData);
+  };
+
+  dataFactory.updateCluster = function (id, name, workerCount) {
+    var jsonData = {
+      "config": {
+        "masterCount": 1,
+        "workerCount": workerCount
+      },
+      "name": name
+    };
+    return $http.put(urlBase + '/clusters/' + id, jsonData);
+  };
+  return dataFactory;
 });
 
-module.factory('sendNotifications', function(Notifications) {
-    var notificationFactory = {};
-    var typeMap = {
-        'Info': Notifications.info,
-        'Success': Notifications.success,
-        'Warning': Notifications.warn,
-        'Error': Notifications.error
-    };
+module.factory('sendNotifications', function (Notifications) {
+  var notificationFactory = {};
+  var typeMap = {
+    'Info': Notifications.info,
+    'Success': Notifications.success,
+    'Warning': Notifications.warn,
+    'Error': Notifications.error
+  };
 
-    notificationFactory.notify = function(type, message) {
-        typeMap[type](message);
-    };
-    return notificationFactory;
+  notificationFactory.notify = function (type, message) {
+    typeMap[type](message);
+  };
+  return notificationFactory;
 });
 
 /* Error handling factory.  Since our server will return
@@ -175,28 +182,28 @@ module.factory('sendNotifications', function(Notifications) {
  * for error handling and can be extended to handle all sorts
  * of things.
  */
-module.factory('errorHandling', function(sendNotifications) {
-    var errorHandlingFactory= {};
+module.factory('errorHandling', function (sendNotifications) {
+  var errorHandlingFactory = {};
 
-    errorHandlingFactory.handle = function(response, error, defer, successMsg) {
-        if (response && response.data && response.data.errors) {
-            response.data.errors.forEach(function (singleError) {
-                console.error(singleError['title'] + "\nStatus Code: " + singleError.status + "\n" + singleError.details);
-            });
-            if (defer) {
-                defer.reject(response.data.errors[0].details);
-            }
-        } else if (error) {
-            console.error("Problem communicating with server.  Error code:  " + error.status);
-            if (defer) {
-                defer.reject(error.data);
-            }
-        } else {
-            sendNotifications.notify("Success", successMsg);
-            if(defer) {
-                defer.resolve(successMsg);
-            }
-        };
-    };
-    return errorHandlingFactory;
+  errorHandlingFactory.handle = function (response, error, defer, successMsg) {
+    if (response && response.data && response.data.errors) {
+      response.data.errors.forEach(function (singleError) {
+        console.error(singleError.title + "\nStatus Code: " + singleError.status + "\n" + singleError.details);
+      });
+      if (defer) {
+        defer.reject(response.data.errors[0].details);
+      }
+    } else if (error) {
+      console.error("Problem communicating with server.  Error code:  " + error.status);
+      if (defer) {
+        defer.reject(error.data);
+      }
+    } else {
+      sendNotifications.notify("Success", successMsg);
+      if (defer) {
+        defer.resolve(successMsg);
+      }
+    }
+  };
+  return errorHandlingFactory;
 });
