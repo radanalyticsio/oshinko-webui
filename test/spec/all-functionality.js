b52eed5dcaeac0e7f3e608445b7d9ec6c3bbb7a9
Chad Roberts <croberts@redhat.com>
2017-08-03 09:27:31 -0400

    Fixing-up integration tests before they get added to travis.

--- test/spec/all-functionality.js
+++ test/spec/all-functionality.js
@@ -31,10 +31,8 @@ describe('Cluster page functionality', function () {
     browser.wait(EC.textToBePresentInElement(element(by.name('workercount-testcluster')), "2"));
 
     // Delete
-    browser.get('');
     element(by.name('deletebutton-testcluster')).click();
     element(by.id('deletebutton')).click();
-    browser.get('');
     browser.wait(EC.invisibilityOf(element(by.name('deletebutton-testcluster'))));
   });
 });
@@ -44,33 +42,35 @@ describe('Cluster page functionality, with additional cancel clicks', function (
   it('should create, scale, and delete a cluster', function () {
     var EC = protractor.ExpectedConditions;
     // Create a cluster
+    browser.wait(EC.elementToBeClickable(element(by.id('startbutton'))));
     element(by.id('startbutton')).click();
+    browser.wait(EC.elementToBeClickable(element(by.id('cancelbutton'))));
     element(by.id('cancelbutton')).click();
+    browser.wait(EC.elementToBeClickable(element(by.id('startbutton'))));
     element(by.id('startbutton')).click();
-    element(by.id('cluster-new-name')).sendKeys('testcluster');
+    element(by.id('cluster-new-name')).sendKeys('secondcluster');
     element(by.id('cancelbutton')).click();
+    browser.wait(EC.elementToBeClickable(element(by.id('startbutton'))));
     element(by.id('startbutton')).click();
-    element(by.id('cluster-new-name')).sendKeys('testcluster');
+    element(by.id('cluster-new-name')).sendKeys('secondcluster');
     element(by.id('createbutton')).click();
-    browser.wait(EC.visibilityOf(element(by.name('deletebutton-testcluster'))));
+    browser.wait(EC.visibilityOf(element(by.name('deletebutton-secondcluster'))));
 
     //Scale
-    element(by.name('scalebutton-testcluster')).click();
+    element(by.name('scalebutton-secondcluster')).click();
     element(by.id('cancelbutton')).click();
-    browser.wait(EC.elementToBeClickable(element(by.name('scalebutton-testcluster'))));
-    element(by.name('scalebutton-testcluster')).click();
+    browser.wait(EC.elementToBeClickable(element(by.name('scalebutton-secondcluster'))));
+    element(by.name('scalebutton-secondcluster')).click();
     element(by.name('numworkers')).sendKeys(protractor.Key.CONTROL, "a", protractor.Key.NULL, "3");
     element(by.id('scalebutton')).click();
-    browser.get('');
-    browser.wait(EC.textToBePresentInElement(element(by.name('workercount-testcluster')), "3"));
+    browser.wait(EC.textToBePresentInElement(element(by.name('workercount-secondcluster')), "3"));
 
     // Delete
-    browser.get('');
-    element(by.name('deletebutton-testcluster')).click();
+    element(by.name('deletebutton-secondcluster')).click();
     element(by.id('cancelbutton')).click();
-    browser.wait(EC.elementToBeClickable(element(by.name('deletebutton-testcluster'))));
-    element(by.name('deletebutton-testcluster')).click();
+    browser.wait(EC.elementToBeClickable(element(by.name('deletebutton-secondcluster'))));
+    element(by.name('deletebutton-secondcluster')).click();
     element(by.id('deletebutton')).click();
-    browser.wait(EC.invisibilityOf(element(by.name('deletebutton-testcluster'))));
+    browser.wait(EC.invisibilityOf(element(by.name('deletebutton-secondcluster'))));
   });
 });
