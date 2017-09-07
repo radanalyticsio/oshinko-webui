7667ecc32073da1771ddbda1124a960c019aacf8
Chad Roberts <croberts@redhat.com>
2017-09-07 13:55:32 -0400

    webui now uses co-podded (or any) oc proxy instance to make api calls. This brings the oshinko-webui more in-line with how oshinko-console works.

--- server.js
+++ server.js
@@ -13,42 +13,29 @@ var app = express();
 var oshinko_proxy_location = process.env.OSHINKO_PROXY_LOCATION || "";
 var oshinko_current_namespace = process.env.CURRENT_NAMESPACE || "";
 
-// This is the default used by the embedded CLI, determined in launch.sh
-// It's used for logging and labeling, never read and used directly
-var spark_default = process.env.SPARK_DEFAULT;
-
-var refresh_interval = process.env.OSHINKO_REFRESH_INTERVAL || 5;
-
 app.configure(function () {
   app.use(express.logger());
   app.set('views', __dirname + '/app');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/app'));
-  app.use('/bower_components', express.static(__dirname + '/app/bower_components'));
+  app.use('/webui/bower_components', express.static(__dirname + '/app/bower_components'));
+  app.use('/webui/css', express.static(__dirname + '/app/css'));
+  app.use('/webui/js', express.static(__dirname + '/app/js'));
+  app.use('/webui/partials', express.static(__dirname + '/app/partials'));
+  app.use('/webui/forms', express.static(__dirname + '/app/forms'));
+  app.use('/webui', express.static(__dirname + '/app'));
   app.use(app.router);
   app.engine('html', require('ejs').renderFile);
 });
 
-app.get('/', function (request, response) {
+app.get('*', function (request, response) {
   response.render('index.html');
-});
-
-app.get('/config/all', function (request, response) {
-  var config = {
-    refresh_interval: refresh_interval,
-    spark_image: spark_default,
-    oshinko_proxy_location: oshinko_proxy_location,
-    oshinko_current_namespace: oshinko_current_namespace
-  };
-  response.send(200, config);
 });
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 app.listen(port, function () {
   console.log("Listening on " + port);
-  console.log("Proxy location: " + oshinko_proxy_location);
-  console.log("Spark default image if not overridden is " + spark_default);
+  console.log("Proxy location: " + oshinko_proxy_location + "/proxy");
   console.log("Current namespace is: " + oshinko_current_namespace);
 });
-
