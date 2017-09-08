e29d0a86237f7508aeae796e331934539e19f60c
Chad Roberts <croberts@redhat.com>
2017-09-08 10:06:54 -0400

    changing label associated with exposed proxy route.

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -3,5 +3,5 @@ export OSHINKO_SA_TOKEN=`cat /var/run/secrets/kubernetes.io/serviceaccount/token
 export OSHINKO_PROXY_LOCATION=`/usr/src/app/oc get routes $WEB_ROUTE_NAME --template={{.spec.host}}`
 echo "The oshinko proxy location is $OSHINKO_PROXY_LOCATION"
 sed -i "s/<PROXY>/'$OSHINKO_PROXY_LOCATION'/" /usr/src/app/app/config.local.js
-/usr/src/app/oc expose service oc-proxy-service --path=/proxy --hostname=$OSHINKO_PROXY_LOCATION
+/usr/src/app/oc expose service oc-proxy-service --path=/proxy --hostname=$OSHINKO_PROXY_LOCATION -l app=oshinko-proxy
 npm start
