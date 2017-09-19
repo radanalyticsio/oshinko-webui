c2c05ec0fc14fa9526fe3c4c5b0ad0bc21d63a4d
Chad Roberts <croberts@redhat.com>
2017-09-19 14:13:32 -0400

    Oshinko-webui now uses console-style calls for all actions.

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Parse the spark default image from the oshinko CLI version info
 # For a version of oshinko-web that doesn't use the CLI, the value would just be set here
 SPARK_DEFAULT=$($(dirname $0)/oshinko version | sed -rn 's/Default spark image:\s(.*)/\1/p')
@@ -11,5 +10,9 @@ export SPARK_DEFAULT
 # Set the text label on advanced cluster create
 sed -i "s@SPARK_DEFAULT@$SPARK_DEFAULT@" app/forms/new-cluster.html
 
-export OSHINKO_SA_TOKEN=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
+export OSHINKO_PROXY_LOCATION=`/usr/src/app/oc get routes $WEB_ROUTE_NAME --template={{.spec.host}}`
+echo "The oshinko proxy location is $OSHINKO_PROXY_LOCATION"
+sed -i "s/<PROXY>/'$OSHINKO_PROXY_LOCATION'/" /usr/src/app/app/config.local.js
+/usr/src/app/oc expose service oc-proxy-service --path=/proxy --hostname=$OSHINKO_PROXY_LOCATION
+
 npm start
