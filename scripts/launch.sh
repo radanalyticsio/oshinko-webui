02a7cde0d4820688f6c492b54f9fe6a29c782dc6
Chad Roberts <croberts@redhat.com>
2017-10-11 14:48:58 -0400

    Updating launch.sh to require fewer parameters from the launch template.

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Parse the spark default image from the oshinko CLI version info
-# For a version of oshinko-web that doesn't use the CLI, the value would just be set here
-SPARK_DEFAULT=$($(dirname $0)/oshinko version | sed -rn 's/Default spark image:\s(.*)/\1/p')
+
 if [ -z "$SPARK_DEFAULT" ]; then
     SPARK_DEFAULT=radanalyticsio/openshift-spark
 fi
@@ -13,6 +11,6 @@ sed -i "s@SPARK_DEFAULT@$SPARK_DEFAULT@" app/forms/new-cluster.html
 export OSHINKO_PROXY_LOCATION=`/usr/src/app/oc get routes $WEB_ROUTE_NAME --template={{.spec.host}}`
 echo "The oshinko proxy location is $OSHINKO_PROXY_LOCATION"
 sed -i "s/<PROXY>/'$OSHINKO_PROXY_LOCATION'/" /usr/src/app/app/config.local.js
-/usr/src/app/oc expose service oc-proxy-service --path=/proxy --hostname=$OSHINKO_PROXY_LOCATION
+/usr/src/app/oc expose service $WEB_ROUTE_NAME-proxy --path=/proxy --hostname=$OSHINKO_PROXY_LOCATION
 
 npm start
