48e85d5c548b029a0a7e43e36ed669a14b6b706a
Chad Roberts <croberts@redhat.com>
2017-11-01 12:37:07 -0400

    Displaying the cluster configuration on the cluster details page.

--- scripts/launch-local.sh
+++ scripts/launch-local.sh
@@ -3,13 +3,15 @@
 
 export CURRENT_NAMESPACE=`oc project -q`
 export OSHINKO_REFRESH_INTERVAL=5
-export OSHINKO_SPARK_IMAGE='radanalyticsio/openshift-spark:latest'
+export SPARK_DEFAULT='radanalyticsio/openshift-spark'
 export OSHINKO_PROXY_LOCATION='127.0.0.1:8001'  #location of oc proxy running
 
-cp ../app/config.template ../app/config.local.js
+export BASEDIR=`git rev-parse --show-toplevel`
+cp $BASEDIR/app/config.template $BASEDIR/app/config.local.js
 
-sed -i "s#<NAMESPACE>#'$CURRENT_NAMESPACE'#" ../app/config.local.js
-sed -i "s#<REFRESH_INTERVAL>#'$OSHINKO_REFRESH_INTERVAL'#" ../app/config.local.js
-sed -i "s#<SPARK_IMAGE>#'$OSHINKO_SPARK_IMAGE'#" ../app/config.local.js
-sed -i "s/<PROXY>/'$OSHINKO_PROXY_LOCATION'/" ../app/config.local.js
+sed -i "s#<NAMESPACE>#'$CURRENT_NAMESPACE'#" $BASEDIR/app/config.local.js
+sed -i "s#<REFRESH_INTERVAL>#'$OSHINKO_REFRESH_INTERVAL'#" $BASEDIR/app/config.local.js
+sed -i "s#<SPARK_IMAGE>#'$SPARK_DEFAULT'#" $BASEDIR/app/config.local.js
+sed -i "s#SPARK_DEFAULT#$SPARK_DEFAULT#" $BASEDIR/app/js/controllers.js
+sed -i "s/<PROXY>/'$OSHINKO_PROXY_LOCATION'/" $BASEDIR/app/config.local.js
 npm start
