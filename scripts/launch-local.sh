5071433a98c4d3ff8f8809310824de67b2147d20
Chad Roberts <croberts@redhat.com>
2017-11-08 09:56:07 -0500

    Fixing up launch-local.sh, also removing config.local.js as it's now generated.

--- scripts/launch-local.sh
+++ scripts/launch-local.sh
@@ -9,9 +9,10 @@ export OSHINKO_PROXY_LOCATION='127.0.0.1:8001'  #location of oc proxy running
 export BASEDIR=`git rev-parse --show-toplevel`
 cp $BASEDIR/app/config.template $BASEDIR/app/config.local.js
 
+sed -i "s#SPARK_DEFAULT#$SPARK_DEFAULT#" $BASEDIR/app/forms/new-cluster.html
 sed -i "s#<NAMESPACE>#'$CURRENT_NAMESPACE'#" $BASEDIR/app/config.local.js
 sed -i "s#<REFRESH_INTERVAL>#'$OSHINKO_REFRESH_INTERVAL'#" $BASEDIR/app/config.local.js
-sed -i "s#<SPARK_IMAGE>#'$SPARK_DEFAULT'#" $BASEDIR/app/config.local.js
+sed -i "s#SPARK_DEFAULT#$SPARK_DEFAULT#" $BASEDIR/app/config.local.js
 sed -i "s#SPARK_DEFAULT#$SPARK_DEFAULT#" $BASEDIR/app/js/controllers.js
 sed -i "s/<PROXY>/'$OSHINKO_PROXY_LOCATION'/" $BASEDIR/app/config.local.js
 npm start
