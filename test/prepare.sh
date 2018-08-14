f66ca235746393b1b85701aea70b6313fb34491c
Chad Roberts <croberts@redhat.com>
2018-08-14 13:56:24 -0400

    Updating the oc install module to discover the filename rather than hard code it.  Also updating the oc binary to version 3.10 in image.yaml. Travis tests will now use openshift origin 3.10.

--- test/prepare.sh
+++ test/prepare.sh
@@ -6,7 +6,7 @@ function prepare() {
   echo "Host IP is $HOST_IP_ADDRESS"
 
   sudo docker cp $(docker create docker.io/openshift/origin:$OPENSHIFT_VERSION):/bin/oc /usr/local/bin/oc
-  oc cluster up --version=$OPENSHIFT_VERSION --public-hostname=$HOST_IP_ADDRESS
+  oc cluster up --public-hostname=$HOST_IP_ADDRESS
   oc login -u system:admin
   export REGISTRY_URL=$(oc get svc -n default docker-registry -o jsonpath='{.spec.clusterIP}:{.spec.ports[0].port}')
   oc login -u developer -p developer
