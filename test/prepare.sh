1108d95afc54a1800f134d1dcf157e0fa9fddc86
Chad Roberts <croberts@redhat.com>
2018-10-16 11:28:50 -0400

    Pick-up v3.10 from docker.  Newer images will come from quay.io.

--- test/prepare.sh
+++ test/prepare.sh
@@ -4,8 +4,11 @@ function prepare() {
   ip addr show eth0
   export HOST_IP_ADDRESS="$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
   echo "Host IP is $HOST_IP_ADDRESS"
-
-  sudo docker cp $(docker create quay.io/openshift/origin-cli:$OPENSHIFT_VERSION):/bin/oc /usr/local/bin/oc
+  IMAGEFOROC="quay.io/openshift/origin-cli:$OPENSHIFT_VERSION"
+  if [ "$OPENSHIFT_VERSION" == "v3.10" ]; then
+      IMAGEFOROC="docker.io/openshift/origin:$OPENSHIFT_VERSION"
+  fi
+  sudo docker cp $(docker create $IMAGEFOROC):/bin/oc /usr/local/bin/oc
   oc cluster up --public-hostname=$HOST_IP_ADDRESS
   oc login -u system:admin
   export REGISTRY_URL=$(oc get svc -n default docker-registry -o jsonpath='{.spec.clusterIP}:{.spec.ports[0].port}')
