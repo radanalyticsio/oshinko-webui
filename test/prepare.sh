6a215700c2d14b56a29e97012d967e8f228e3337
Chad Roberts <croberts@redhat.com>
2018-10-16 10:38:12 -0400

    Updating to use quay.io instead of docker.io

--- test/prepare.sh
+++ test/prepare.sh
@@ -5,7 +5,7 @@ function prepare() {
   export HOST_IP_ADDRESS="$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
   echo "Host IP is $HOST_IP_ADDRESS"
 
-  sudo docker cp $(docker create docker.io/openshift/origin:$OPENSHIFT_VERSION):/bin/oc /usr/local/bin/oc
+  sudo docker cp $(docker create quay.io/openshift/origin:$OPENSHIFT_VERSION):/bin/oc /usr/local/bin/oc
   oc cluster up --public-hostname=$HOST_IP_ADDRESS
   oc login -u system:admin
   export REGISTRY_URL=$(oc get svc -n default docker-registry -o jsonpath='{.spec.clusterIP}:{.spec.ports[0].port}')
